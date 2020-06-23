6e1c39c7fd2ab11fdba1a8096d254ec1fc331711
googlx <zenghui@tom.com>
2020-06-23 12:42:20 +0800

    计算ROUGE

--- rouge.py
+++ rouge.py
@@ -1,8 +1,8 @@
 from utils_rouge import rouge_eval, rouge_log
 
 
-rouge_ref_dir = '../output/rouge_ref'
-rouge_dec_dir = '../output/rouge_dec'
+rouge_ref_dir = './output/reference'
+rouge_dec_dir = './output/decoded'
 print("Decoder has finished reading dataset for single_pass.")
 print("Now starting ROUGE eval...")
 results_dict = rouge_eval(rouge_ref_dir, rouge_dec_dir)
--- utils_rouge.py
+++ utils_rouge.py
@@ -20,11 +20,11 @@ def make_html_safe(s):
 
 def rouge_eval(ref_dir, dec_dir):
   r = pyrouge.Rouge155()
-  r.model_filename_pattern = '#ID#_reference.txt'
-  r.system_filename_pattern = '(\d+)_decoded.txt'
+  r.model_filename_pattern = '#ID#.txt'
+  r.system_filename_pattern = '(\d+).txt'
   r.model_dir = ref_dir
   r.system_dir = dec_dir
-  logging.getLogger('global').setLevel(logging.WARNING) # silence pyrouge logging
+  logging.getLogger('global').setLevel(logging.INFO) # silence pyrouge logging
   rouge_results = r.convert_and_evaluate()
   return r.output_to_dict(rouge_results)
 
