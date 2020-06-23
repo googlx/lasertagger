ad89868b094496c412b629356a7ad51423b1dbb5
googlx <zenghui@tom.com>
2020-06-23 11:52:59 +0800

    add rouge

---
+++ rouge.py
@@ -0,0 +1,9 @@
+from utils import rouge_eval, rouge_log
+
+
+rouge_ref_dir = '../output/rouge_ref'
+rouge_dec_dir = '../output/rouge_dec'
+print("Decoder has finished reading dataset for single_pass.")
+print("Now starting ROUGE eval...")
+results_dict = rouge_eval(rouge_ref_dir, rouge_dec_dir)
+rouge_log(results_dict, './output')
